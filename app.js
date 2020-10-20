4460bd968e25290d7b014ba66dc54f81337e0816
Yossi Shirizli <yossi.shirizli@gmail.com>
2020-10-20 23:52:00 +0300

    basic routes

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+//middleware - put the data from the body on the request object
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({
-      app: 'Movie Collector',
-      status: 'success',
-      message: 'movies hello',
-    });
+const movies = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/movies.json`));
+app.get('/api/v1/movies', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: movies.length,
+    data: {
+      movies,
+    },
+  });
+});
+app.post('/api/v1/movies', (req, res) => {
+  const newId = movies[movies.length - 1].id + 1;
+  const newMovie = Object.assign({ id: newId }, req.body);
+  movies.push(newMovie);
+  fs.writeFile(
+    `${__dirname}/dev-data/movies.json`,
+    JSON.stringify(movies),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          movie: newMovie,
+        },
+      });
+    }
+  );
+  res.send('Done');
 });
 
 const port = 3000;

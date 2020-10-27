43e95c4c2f4c5849235d88a3743b762fa2ddc7b8
Yossi Shirizli <yossi.shirizli@gmail.com>
2020-10-27 20:45:36 +0200

    support undefined routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const morgan = require('morgan');
 
@@ -19,4 +20,11 @@ app.use(express.json());
 // **** ROUTES
 app.use('/api/v1/movies', movieRouter);
 
+//undefined routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on the server`
+  });
+});
 module.exports = app;

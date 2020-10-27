ec79cca84becdaf9fd86c77e78b6c4774674dcd3
Yossi Shirizli <yossi.shirizli@gmail.com>
2020-10-27 18:20:00 +0200

    alignment

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,61 +1,52 @@
 const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema(
-    {
-        jobType: {
-            type: String,
-            required: [true, 'A job must have a type'],
-            enum: {
-            values: [
-                'retriveMovieInfo',
-                'retruveReleaseInfo'
-            ],
-            message:
-                'Type is either: retriveMovieInfo or retruveReleaseInfo'
-            }
-        },
-        data:{
-            type: String,
-            required:[true, 'A job must have data']
-        },
-        CreateAt:{
-            type:Date,
-            default:Date.now()
-        },
-        finishedAt:{
-            type:Date,
-            default:undefined
-        },
-        status: {
-            type: String,
-            enum: {
-                values: [
-                'Pending',
-                'Executing',
-                'Completed',
-                'Abourted',
-                'Failed'
-                ],
-                message: 'Status is either: Pending, Executing, Completed, Abourted or Failed'
-            },
-            default:'Pending'
-        },
-        reason:{
-            type:String,
-            default:undefined
-        },
+  {
+    jobType: {
+      type: String,
+      required: [true, 'A job must have a type'],
+      enum: {
+        values: ['retriveMovieInfo', 'retruveReleaseInfo'],
+        message: 'Type is either: retriveMovieInfo or retruveReleaseInfo'
+      }
     },
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
+    data: {
+      type: String,
+      required: [true, 'A job must have data']
+    },
+    CreateAt: {
+      type: Date,
+      default: Date.now()
+    },
+    finishedAt: {
+      type: Date,
+      default: undefined
+    },
+    status: {
+      type: String,
+      enum: {
+        values: ['Pending', 'Executing', 'Completed', 'Abourted', 'Failed'],
+        message:
+          'Status is either: Pending, Executing, Completed, Abourted or Failed'
+      },
+      default: 'Pending'
+    },
+    reason: {
+      type: String,
+      default: undefined
     }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
 // Virtuals
 jobSchema.virtual('duration').get(function() {
   if (!this.finishedAt) return 'N/A';
 
-  const duration=this.finishedAt-this.CreateAt;
+  const duration = this.finishedAt - this.CreateAt;
   return duration;
 });
 

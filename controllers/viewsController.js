bed852d79955b4f884589a1c383b8b99c071215e
Yossi Shirizli <yossi.shirizli@gmail.com>
2020-11-11 21:04:08 +0200

    basic web pages

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,16 +1,35 @@
-const Movie = require('./../model/tourModel');
+const Movie = require('./../models/movieModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1. get movies data from db
-  const movie = await Movie.find();
+  // const movies = await Movie.find();
+  const query = Movie.find().sort('title releaseDate');
+  const movies = await query;
 
   // 2. buid template
-
+  // res.status(200).render('base');
   // 3. render template using tours data
   res.status(200).render('overview', {
     title: 'All Movies',
     movies
+  });
+});
+
+exports.getMovie = catchAsync(async (req, res, next) => {
+  // const movie = await Movie.findOne({ slug: req.params.slug }).populate({
+  //   path: 'reviews',
+  //   fields: 'review rating user',
+  // });
+  const movie = await Movie.findOne({ slug: req.params.slug });
+
+  if (!movie) {
+    return next(new AppError('There is no movie with that name', 404));
+  }
+
+  res.status(200).render('movie', {
+    title: `${movie.title}`,
+    movie
   });
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const viewsController = require('./../controllers/viewsController');
+const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
+router.get('/movie/:slug', viewsController.getMovie);
 
 module.exports = router;
